5ba382830c48de29357e3648d12ea607528d4a1a
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 14:22:27 +0100

    refactor the condition in the print

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 for numb in range(0, 100):
-    if numb == 99:
-        print(f"{numb}")
-
-    else:
-        numb = "%02d" % (numb) + ", "
-        print(f"{numb}", end="")
+    print(f"{numb:02d}", end=", " if numb != 99 else "\n")
