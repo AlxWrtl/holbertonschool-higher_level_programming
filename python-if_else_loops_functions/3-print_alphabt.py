887a27090e01db55a9d5ccf6079711e1506c3615
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 19:03:45 +0100

    add the ord function to exclude the lettre q and e

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for ASCII in range(97, 123):
-    if ASCII != 'e' and ASCII != 'q':
+    if ASCII != ord("e") and ASCII != ord("q"):
         print("{}".format(chr(ASCII)), end="")
