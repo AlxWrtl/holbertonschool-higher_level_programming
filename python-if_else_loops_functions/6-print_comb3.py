dd8aac0feb72ca3c324d0c9b4d7dd4ba25445a5e
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 14:46:19 +0100

    no comma after 89

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for numb in range(0, 100):
         if numb // 10 < numb % 10:
-            print("{:02d}".format(numb), end=", " if numb != 99 else "\n")
+            print("{:02d}".format(numb), end=", " if numb != 89 else "\n")
