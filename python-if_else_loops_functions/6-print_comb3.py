c037ea6b6f7e50555bff2f04f86b72e4d3e04cde
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 14:45:33 +0100

    refactor the condition and the print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for numb1 in range(0, 10):
-    for numb2 in range(numb1 + 1, 10):
-        if numb1 < 9 or numb2 < 9:
-            print("%02d, " % (numb1 * 10 + numb2), end="")
-        else:
-            print("%02d" % (numb1 * 10 + numb2))
+for numb in range(0, 100):
+        if numb // 10 < numb % 10:
+            print("{:02d}".format(numb), end=", " if numb != 99 else "\n")
