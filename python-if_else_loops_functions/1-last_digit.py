826401b453f52d15cfdf2c5c90ccf3388cba53e2
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 13:59:47 +0100

    change id its negative number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,13 @@
 import random
 
 number = random.randint(-10000, 10000)
-lastDigit = int(repr(number)[-1])
+lastDigit = abs(number) % 10
+if number < 0:
+    lastDigit = -lastDigit
 
-if lastDigit > 5:
+elif number > 0 and lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
-if lastDigit == 0:
+elif lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
 if lastDigit < 6 and lastDigit != 0:
-    print(f"Last digit of {number} is {lastDigit} and is less than 6")
+    print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
