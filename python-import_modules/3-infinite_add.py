a46af859f31d8ce201af0aca780e04f2d3fcc943
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 15:33:06 +0100

    add if __name__ == "__main__":

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 import sys
 
-argv = sys.argv[1:]
-arg = len(argv)
-result = 0
+if __name__ == "__main__":
 
-for value in range(0, len(argv)):
-    argv[value] = int(argv[value])
-    numbers = int(argv[value])
-    result += numbers
+    argv = sys.argv[1:]
+    arg = len(argv)
+    result = 0
 
-if result == 0:
-    print("0")
+    for value in range(0, len(argv)):
+        argv[value] = int(argv[value])
+        numbers = int(argv[value])
+        result += numbers
 
-else:
-    print(result)
+    if result == 0:
+        print("0")
+
+    else:
+        print(result)
 
