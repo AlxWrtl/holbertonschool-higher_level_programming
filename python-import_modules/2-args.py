1c7e56eec5b557cb6af55d73050769f1eaee2266
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 11:16:21 +0100

    add the condition if it's one arg

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,11 @@ arg = len(argv)
 if arg == 0:
     print(f"{arg} arguments.")
 
+elif arg == 1:
+    print(f"{arg} argument:")
+    for i, arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
+
 else:
     print(f"{arg} arguments:")
     for i, arg in enumerate(argv, start=1):
