111a559a1a8b367bd49c7b811055f97afd5269b0
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 01:42:22 +0100

    change the maner to creat the function to print

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
-NewStr = str.replace(f"{str}", "object-oriented programming with Python",)
+NewStr = str[39:66] + " " + str[-22:-18] + " " + str[:6]
 print(NewStr)
