3400437cd16f109d91f3d9d15a2140b197120f96
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 15:30:49 +0100

    add the prevent the script from running when imported

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 import sys
 
-argv = sys.argv[1:]
-arg = len(argv)
+if __name__ == "__main__":
 
-if arg == 0:
-    print("{} arguments.".format(arg), end="")
+    argv = sys.argv[1:]
+    arg = len(argv)
 
-elif arg == 1:
-    print("{} argument:".format(arg))
-    for i, arg in enumerate(argv, start=1):
-        print("{}: {}".format(i, arg))
+    if arg == 0:
+        print("{} arguments.".format(arg), end="")
 
-else:
-    print("{} arguments:".format(arg))
-    for i, arg in enumerate(argv, start=1):
-        print("{}: {}".format(i, arg))
+    elif arg == 1:
+        print("{} argument:".format(arg))
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, arg))
+
+    else:
+        print("{} arguments:".format(arg))
+        for i, arg in enumerate(argv, start=1):
+            print("{}: {}".format(i, arg))
