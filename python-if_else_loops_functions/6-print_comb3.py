f0ee791a0de0fc3c0d287de7c9d119bc9cb4e0f9
SuperPims <indexes-benzine0p@icloud.com>
2024-01-24 10:19:16 +0100

    change the condition if

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for numb1 in range(0, 10):
     for numb2 in range(numb1 + 1, 10):
-        if numb1 == 8 and numb2 == 9:
-            print("%02d" % (numb1 * 10 + numb2))
-        else:
+        if numb1 < 9 or numb2 < 9:
             print("%02d, " % (numb1 * 10 + numb2), end="")
+        else:
+            print("%02d" % (numb1 * 10 + numb2))
