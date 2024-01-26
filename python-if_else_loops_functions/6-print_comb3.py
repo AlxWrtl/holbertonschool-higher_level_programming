629c58861b7d7670d9140aea107641a9a92d9d2f
SuperPims <indexes-benzine0p@icloud.com>
2024-01-26 09:33:03 +0100

    correct for pycodstyle

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for numb in range(0, 100):
-        if numb // 10 < numb % 10:
-            print("{:02d}".format(numb), end=", " if numb != 89 else "\n")
+    if numb // 10 < numb % 10:
+        print("{:02d}".format(numb), end=", " if numb != 89 else "\n")
