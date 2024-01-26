50319088f7a36ac04238b9fda1fdb34c95c84911
SuperPims <indexes-benzine0p@icloud.com>
2024-01-26 09:20:34 +0100

    change the confidtion elif by if

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ lastDigit = abs(number) % 10
 if number < 0:
     lastDigit = -lastDigit
 
-elif number > 0 and lastDigit > 5:
+if number > 0 and lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
-elif lastDigit == 0:
+if lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
 if lastDigit < 6 and lastDigit != 0:
     print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
