2e0257b4c200d5348169774e2f606e6d39ecb64e
SuperPims <indexes-benzine0p@icloud.com>
2024-01-26 20:31:42 +0100

    refactor the code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,18 +3,12 @@ import sys
 
 if __name__ == "__main__":
 
-    argv = sys.argv[1:]
-    arg = len(argv)
-
-    if arg == 0:
-        print("{} arguments.".format(arg), end="")
-
-    elif arg == 1:
-        print("{} argument:".format(arg))
-        for i, arg in enumerate(argv, start=1):
-            print("{}: {}".format(i, arg))
-
+    num_args = len(sys.argv) - 1
+    if (num_args == 0):
+        print("0 arguments.")
+    elif (num_args == 1):
+        print("1 argument:")
     else:
-        print("{} arguments:".format(arg))
-        for i, arg in enumerate(argv, start=1):
-            print("{}: {}".format(i, arg))
+        print("{} arguments:".format(num_args))
+    for arg in range(1, len(sys.argv)):
+        print("{}: {}".format(arg, sys.argv[arg]))
