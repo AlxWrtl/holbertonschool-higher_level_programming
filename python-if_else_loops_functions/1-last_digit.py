0655051e70dc431a9945a77fac078d7c4bcb35d9
SuperPims <indexes-benzine0p@icloud.com>
2024-01-26 09:23:43 +0100

    change some if by elif and the first condition by just if >5

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if number < 0:
 
 if number > 0 and lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
-if lastDigit == 0:
+elif lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
-if lastDigit < 6 and lastDigit != 0:
+elif lastDigit < 6 and lastDigit != 0:
     print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
