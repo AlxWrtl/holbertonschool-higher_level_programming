935a41753a3d0062fb9ae3af0ee7546fbe0a72b5
SuperPims <indexes-benzine0p@icloud.com>
2024-01-31 18:17:29 +0100

    Implement function to return first character and length of a sentence

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+
+    if sentence == "":
+        first = None
+    else:
+        first = sentence[0]
+
+    lenght = len(sentence)
+    return first, lenght
