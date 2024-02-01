f179de3abca922bf5186956285dbbef05dcf12bd
SuperPims <indexes-benzine0p@icloud.com>
2024-02-01 09:31:35 +0100

    refactor the code

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    new_list = my_list[:]
+    list_copy = my_list[:]
 
-    if idx < len(my_list[:]):
-        my_list[idx] = element
-    return new_list
+    if 0 <= idx < len(list_copy):
+        list_copy[idx] = element
+    return list_copy
