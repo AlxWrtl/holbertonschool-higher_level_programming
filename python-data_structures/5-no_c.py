1ea866e7436cc59555ff798837eaedcb6e655d5c
SuperPims <indexes-benzine0p@icloud.com>
2024-02-01 09:38:04 +0100

    refactor

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-    new_string = my_string
-    my_string = ""
-    for char in my_string:
-        if char != "c" and char != "C":
-            new_string += char
+    new_string = "".join(char for char in my_string if char not in ["c", "C"])
     return new_string
