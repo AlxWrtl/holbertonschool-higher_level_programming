21414cf4e174bd1cfa4b831e7c68627b9d296746
SuperPims <indexes-benzine0p@icloud.com>
2024-02-01 09:47:52 +0100

    refactor

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
 
-    results = []
-
-    for i in my_list:
-        if i % 2 == 0:
-            results.append(True)
-    else:
-        results.append(False)
-    return results
+    return [num % 2 == 0 for num in my_list]
