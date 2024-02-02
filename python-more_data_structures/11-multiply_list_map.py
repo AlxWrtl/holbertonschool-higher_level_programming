2db9a8415c88e35f3f6748db44dcc1b05df45f72
SuperPims <indexes-benzine0p@icloud.com>
2024-02-02 11:14:08 +0100

    del the condition

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda mult: mult * number, my_list if my_list is not None else []))
+    return list(map(lambda mult: mult * number, my_list))
