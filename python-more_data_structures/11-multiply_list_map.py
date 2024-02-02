3563395cb567d9222c9685adf44a10c2244531ea
SuperPims <indexes-benzine0p@icloud.com>
2024-02-02 10:52:16 +0100

    refactor to just have 3 lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
 
-    if my_list is None:
-        my_list = []
-    return list(map(lambda mult: mult * number, my_list))
+    return list(map(lambda mult: mult * number, my_list if my_list is not None else []))
