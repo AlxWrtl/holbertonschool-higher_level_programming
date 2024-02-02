ab1a32eeea0d70936d229e4e5a21356d7aed6b1d
SuperPims <indexes-benzine0p@icloud.com>
2024-02-02 09:47:34 +0100

    fix pycodestyle length of line 4

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
 
-
-    new_dictionary = {key: a_dictionary[key] * 2 for key in sorted(a_dictionary)}
+    old_one = a_dictionary
+    new_dictionary = {key: old_one[key] * 2 for key in sorted(old_one)}
     return new_dictionary
