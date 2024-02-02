112984d36dff3b14feeb0b96ced29ae8ed43599c
SuperPims <indexes-benzine0p@icloud.com>
2024-02-02 20:35:38 +0100

    first character = None

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        first = None
+        sentence[0] = None
     else:
         first = sentence[0]
     length = len(sentence)
