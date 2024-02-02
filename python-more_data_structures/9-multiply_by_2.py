9ca458b0f7a20fb1891f0cbad6c1613468df3ad5
SuperPims <indexes-benzine0p@icloud.com>
2024-02-02 09:44:10 +0100

    sorted new_dictionary

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
 
-    new_dictionary = {key: value * 2 for key, value in a_dictionary.items()}
+
+    new_dictionary = {key: a_dictionary[key] * 2 for key in sorted(a_dictionary)}
     return new_dictionary
