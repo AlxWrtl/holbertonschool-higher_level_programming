841660c79bd8ceb3a8aab4f8324949a6c5adefee
SuperPims <indexes-benzine0p@icloud.com>
2024-02-02 16:20:40 +0100

    del the indexerror

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             int_count += 1
         except (ValueError, TypeError):
             continue
-        except IndexError:
-            print()
-            raise
     print()
     return int_count
