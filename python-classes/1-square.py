94514c85901960e66064d4827e3fd3d50e967600
SuperPims <indexes-benzine0p@icloud.com>
2024-02-07 16:39:04 +0100

    Implement Square class with initializer

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for Square class definition.
+
+This module defines a Square class for representing squares with a
+customizable size. It includes an initializer to set the size of the square.
+"""
+
+
+class Square:
+    """Represents a square with customizable size."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance with a specific size.
+
+        Args:
+            size: The size of the square's sides.
+        """
+        self.__size = size 
