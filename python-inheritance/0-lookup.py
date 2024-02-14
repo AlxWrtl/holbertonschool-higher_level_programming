fbfb6b47a5a3e16f22d8e95f30b95ec3ccd42407
SuperPims <indexes-benzine0p@icloud.com>
2024-02-14 20:53:56 +0100

    Add utility function for object introspection with dir()

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""Module providing a utility function to list object attributes and methods.
+"""
+
+
 def lookup(obj):
-    """returns the list of available attributes and methods of an object"""
+    """Return the list of available attributes and methods of `obj`.
+
+    This function utilizes the built-in `dir()` function to retrieve a sorted
+    list of attributes and methods belonging to the specified object. It's
+    useful for introspection and exploring the capabilities of an object
+    dynamically at runtime.
+
+    Args:
+        obj (object): The object to introspect.
+
+    Returns:
+        list: A list of strings representing the object's attributes and
+        methods.
+    """
     return dir(obj)
