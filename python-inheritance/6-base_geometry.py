e929f8e848372677eca2c3a6f311339a4057913b
SuperPims <indexes-benzine0p@icloud.com>
2024-02-15 08:19:16 +0100

    Extend BaseGeometry with unimplemented area method for subclass overriding

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
+"""Enhances the BaseGeometry class by adding an area method.
+
+This module extends the BaseGeometry class to include an 'area' method,
+which is intended to be overridden in subclasses. It raises an exception
+if directly called, enforcing implementation in derived classes.
+"""
+
+
 class BaseGeometry:
-    """Class BaseGeometry"""
+    """A class that serves as a foundation for geometric shapes with a
+    placeholder 'area' method.
+    """
+
     def area(self):
-        """Method that raises an Exception with the message area() is not implemented"""
+        """Placeholder method to compute the area of a shape.
+
+        Raises:
+            Exception: If the method is not implemented in a subclass.
+        """
         raise Exception("area() is not implemented")
