de521bab689234af0fb5dab133028c7724bfc7dc
SuperPims <indexes-benzine0p@icloud.com>
2024-02-15 08:44:36 +0100

    change the except by just return

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    """Check if an object is exactly an instance of a specified class.
+    """Determine if an object is exactly an instance of a given class.
 
-    This function determines if the object `obj` is an instance of the class
-    `a_class` directly, not considering inheritance from subclasses.
+    Simplifies the check to a single line by directly returning the result of
+    the comparison between the object's type and the specified class. This
+    approach eliminates the need for explicit if-else statements.
 
     Args:
-        obj: The object to check.
-        a_class: The class to check against.
+        obj: The object to be checked.
+        a_class: The class to compare against.
 
     Returns:
-        bool: True if `obj` is exactly an instance of `a_class`, False otherwise.
+        bool: True if `obj` is exactly an instance of `a_class`, otherwise False.
     """
-    if type(obj) is a_class:
-        return True
-    else:
-        return False
+    return type(obj) is a_class
