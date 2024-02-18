1f57eab53f97d2d92e3af85b4cb8eb2ea71fbd5f
SuperPims <indexes-benzine0p@icloud.com>
2024-02-18 17:40:23 +0100

    Implement script to aggregate command-line arguments in a JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
-class Student:
-    def __init__(self, first_name, last_name, age):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
+"""Script to add all arguments to a Python list and save them to a file.
 
-    def to_json(self):
-    return self.__dict__
+This script takes all command-line arguments provided to it, adds them to a
+list, and then saves the list to a JSON file. If the JSON file does not exist,
+it creates a new list; otherwise, it appends to the existing list.
+"""
+
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+file_name = "add_item.json"  # Define the name of the JSON file to use
+
+try:
+    json_file = load_from_json_file(file_name)
+except FileNotFoundError:
+    json_file = []
+
+json_file.extend(sys.argv[1:])
+
+save_to_json_file(json_file, file_name)
