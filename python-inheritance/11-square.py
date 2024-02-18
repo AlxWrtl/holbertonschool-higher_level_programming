09331dfd311d19aaae5ef61f2d27b6ed9d627a12
SuperPims <indexes-benzine0p@icloud.com>
2024-02-18 17:19:23 +0100

    dell an import

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 Rectangle = __import__('9-rectangle').Rectangle
-Square = __import__('10-square').Square
-
 
 class Square(Rectangle):
     """Inherits from Rectangle to represent a square.
@@ -26,5 +24,4 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
-
+        self.size = size
