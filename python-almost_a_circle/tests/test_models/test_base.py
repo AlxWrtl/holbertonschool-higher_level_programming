8ea6f748c1869a4e063c2f09ed579dcb3ae8ab97
SuperPims <indexes-benzine0p@icloud.com>
2024-02-22 13:40:56 +0100

    test string

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,6 +10,11 @@ class TestBase(unittest.TestCase):
         test = Base(50)
         self.assertEqual(test.id, 50)
 
+    def test_assign_id(self):
+            test = Base("50")
+            self.assertEqual(test.id, "50")
+
+
     def test_no_id_assigned(self):
         test = Base()
         self.assertEqual(test.id, 1)
