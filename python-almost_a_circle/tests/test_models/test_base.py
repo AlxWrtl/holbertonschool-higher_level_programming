3f21cf4eca907b3ee8939b2f309240fa4144a542
SuperPims <indexes-benzine0p@icloud.com>
2024-02-22 15:30:05 +0100

    changes

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -10,30 +10,25 @@ class TestBase(unittest.TestCase):
         test = Base(50)
         self.assertEqual(test.id, 50)
 
-    def test_assign_id(self):
-            test = Base("50")
-            self.assertEqual(test.id, "50")
-
-
     def test_no_id_assigned(self):
         test = Base()
         self.assertEqual(test.id, 1)
 
     def test_to_json_string(self):
         dictionary = {'id': 50,
-                    'width': 10,
-                    'height': 5,
-                    'x': 2,
-                    'y': 3}
+                      'width': 10,
+                      'height': 5,
+                      'x': 2,
+                      'y': 3}
         json_string = Base.to_json_string(dictionary)
         self.assertTrue(isinstance(json_string, str))
 
     def test_to_json_string_content(self):
         dictionary = {'id': 50,
-                    'width': 10,
-                    'height': 5,
-                    'x': 2,
-                    'y': 3}
+                      'width': 10,
+                      'height': 5,
+                      'x': 2,
+                      'y': 3}
         json_string = Base.to_json_string(dictionary)
         self.assertCountEqual(json.loads(json_string), dictionary)
 
@@ -59,5 +54,4 @@ class TestBase(unittest.TestCase):
     def test_create_error(self):
         with self.assertRaises(Exception) as err:
             Base.create()
-        # change error msg to your own
-        self.assertEqual(str(err.exception), "Wrong class")
+        self.assertEqual(str(err.exception), "Wrong class") # change error msg to your own
