17dbab1f8ecd91357dd13f3bb54a10d96a026c24
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-16 22:38:21 +0100

    script that lists all the tables of a database

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,4 @@
+-- Execute SHOW TABLES command on the specified database
+-- This command lists all tables in the database 'database_name'
+-- The user 'username' will be prompted for their password due to the '-p' flag
+mysql - u username - p - D database_name - e "SHOW TABLES;"
