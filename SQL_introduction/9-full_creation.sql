c8670fbe66f519184651551cb3ca7fe10f5c9bc4
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-17 11:41:40 +0100

    change the set value by insert into and set

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,35 @@
--- Create a new table named 'second_table' if it does not already exist
--- The table includes three columns: 'id' as an integer with PRIMARY KEY, 'name' as a VARCHAR(256), and 'score' as an integer
-CREATE TABLE IF NOT EXISTS second_table (id INT PRIMARY KEY, name VARCHAR(256), score INT);
+-- Creates a table named 'second_table' if it does not exist with columns 'id' (INT), 'name' (VARCHAR(256)), and 'score' (INT)
+CREATE TABLE
+    IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
--- Insert multiple rows into 'second_table' with specified values for 'id', 'name', and 'score'
-INSERT INTO second_table (id, name, score)
-VALUES
-    (1, 'John', 10),
-    (2, 'Alex', 3),
-    (3, 'Jhon', 14),
-    (4, 'George', 8);
+-- Inserts a row into 'second_table' with id = 1, name = 'John', and score = 10
+INSERT INTO
+    second_table
+SET
+    id = 1,
+    name = 'John',
+    score = 10;
+
+-- Inserts a row into 'second_table' with id = 2, name = 'Alex', and score = 3
+INSERT INTO
+    second_table
+SET
+    id = 2,
+    name = 'Alex',
+    score = 3;
+
+-- Inserts a row into 'second_table' with id = 3, name = 'Bob', and score = 14
+INSERT INTO
+    second_table
+SET
+    id = 3,
+    name = 'Bob',
+    score = 14;
+
+-- Inserts a row into 'second_table' with id = 4, name = 'George', and score = 8
+INSERT INTO
+    second_table
+SET
+    id = 4,
+    name = 'George',
+    score = 8;
