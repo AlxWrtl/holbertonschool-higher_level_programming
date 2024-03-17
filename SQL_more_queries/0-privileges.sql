eebc356c9565e0a6bc7619af2560ba2ce70abec7
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-17 22:56:03 +0100

    add the comments

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,5 @@
+-- Display the grants (permissions) for the user 'user_0d_1' connecting from 'localhost'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Display the grants (permissions) for the user 'user_0d_2' connecting from 'localhost'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
