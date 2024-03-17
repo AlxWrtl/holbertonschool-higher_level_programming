7f20f85277821308d88247fc3fdafc349cf97d83
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-17 10:34:02 +0100

    add to id primary key so it dont multiply the ids

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- Create a new table named 'second_table' if it does not already exist
--- The table includes three columns: 'id' as an integer, 'name' as a VARCHAR(256), and 'score' as an integer
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+-- The table includes three columns: 'id' as an integer with PRIMARY KEY, 'name' as a VARCHAR(256), and 'score' as an integer
+CREATE TABLE IF NOT EXISTS second_table (id INT PRIMARY KEY, name VARCHAR(256), score INT);
 
 -- Insert multiple rows into 'second_table' with specified values for 'id', 'name', and 'score'
 INSERT INTO second_table (id, name, score)
