a7622bd3ea96535305aba05b0690c8ce2587f5c7
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-17 12:31:42 +0100

    add sorted by the number of records (descending)

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,9 +1,12 @@
--- Retrieve the 'score' and the count of occurrences of each 'score' in 'second_table'
--- Group the results by the 'score' column
+-- List the number of records with the same score in the 'second_table' of the 'hbtn_0c_0' database
+-- Display 'score' and the count of records for each score labeled as 'number'
+-- Sort the list by the count of records (number) in descending order
 SELECT
     score,
     COUNT(*) AS number
 FROM
     second_table
 GROUP BY
-    score;
+    score
+ORDER BY
+    number DESC;
