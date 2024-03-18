585ce9741fbf97aa00867e8aa09994be75c3d0f9
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-18 14:24:26 +0100

    create a new database + a new user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- Create the database if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user if they do not already exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on the database to the user
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Apply the changes
+FLUSH PRIVILEGES;
