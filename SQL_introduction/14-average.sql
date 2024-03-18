299b9609328561cc3de5a3fae2ecaeb643aa71fe
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-18 14:22:02 +0100

    add the comment

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,1 +1,2 @@
-SELECT AVG(score) as average from second_table
+-- Calculate the average of the 'score' column from the 'second_table' and label it as 'average'
+SELECT AVG(score) AS average FROM second_table;
