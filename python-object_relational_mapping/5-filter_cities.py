7de8a91529a91ebd40258992d7b01869874135a1
AlxWrtl <indexes-benzine0p@icloud.com>
2024-03-22 22:58:48 +0100

    fic bugs

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,42 +1,43 @@
 #!/usr/bin/python3
 """
-Module for listing cities of a specified state from a database.
-
-This script accepts the name of a state as an argument and lists all cities
-of that state from the 'hbtn_0e_4_usa' database. It demonstrates secure
-database interaction using MySQLdb, with command-line arguments for database
-credentials and the state name. The script ensures safe query execution with
-parameterized queries to prevent SQL injection.
+script that takes in the name of a state as an argument
+and lists all cities of that state,
+using the database hbtn_0e_4_usa.
 """
-
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
-    """
-    Connects to the MySQL database using credentials and database name
-    provided as command-line arguments. Retrieves cities belonging to
-    a specified state.
-    """
+
+if __name__ == "__main__":
+    # Connect to MySQL server
     db = MySQLdb.connect(
-        host="localhost",  # Database server address
-        user=argv[1],      # Database username
-        port=3306,         # Database server port
-        passwd=argv[2],    # Database password
-        db=argv[3]         # Database name
-    )
+        host="localhost",   # Host name to DB server
+        user=argv[1],       # Username passed in argument
+        port=3306,          # Port number of the DB server
+        passwd=argv[2],     # Pwd passed in argument
+        db=argv[3]
+    )         # DataBase passe in argument
 
-    # Execute a query to fetch cities of a given state in a secure manner
+    # create cursor object associated to query, to acts as a pointer
+    # or iterator that allows us to traverse the result
+
+    query = """
+        SELECT cities.id, cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name LIKE BINARY %(state_name)s
+        ORDER BY cities.id ASC"""
+
     with db.cursor() as cursor:
-        cursor.execute("""
-                    SELECT cities.name
-                FROM cities JOIN states ON cities.state_id = states.id
-                WHERE states.name LIKE BINARY %(state_name)s
-                ORDER BY cities.id ASC""", {'state_name': argv[4]})
+        cursor.execute(query, {'state_name': argv[4]})
 
-        # Fetch all rows returned by the query
         rows = cursor.fetchall()
 
-    # Print the names of the cities for the specified state
-    if rows:
-        print(", ".join([row[0] for row in rows]))
+    if rows is not None:
+        # Print the names of the cities separated by commas
+        print(", ".join([row[1] for row in rows]))
+
+    # Close all cursors
+    cursor.close()
+    # Close all databases
+    db.close()
